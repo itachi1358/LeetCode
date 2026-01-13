ca04a1c07401ed7880cb473e9478ab107495ffb9
Arshad <70105497+itachi1358@users.noreply.github.com>
2026-01-13 14:41:09 +0530

    LC-zigzag-conversion [Runtime: N/A; Memory: N/A]

---
+++ string/zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<vector<char>> v(numRows,vector<char> (1000,'-'));
+        int idx=0,i=0,j=0;
+        bool diagonal=0;
+        while(idx<s.size()){
+            v[i][j]=s[idx];
+            if(diagonal){
+                j++;
+                i--;
+            }
+            else{
+                i++;
+            }
+
+            if(i==numRows-1){
+                diagonal=true;
+            }
+
+            if(i==0) diagonal=false;
+            idx++;
+        }
+        string ans="";
+        for(auto row:v){
+            for(auto ch:row){
+                if(ch!='-')ans+=ch;
+            }
+        }
+
+        return ans;
+    }
+};
