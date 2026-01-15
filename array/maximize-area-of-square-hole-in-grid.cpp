b9322dda9da7dab666fa4b49bd0d73bd8edbec58
Arshad <70105497+itachi1358@users.noreply.github.com>
2026-01-15 13:25:32 +0530

    LC-maximize-area-of-square-hole-in-grid [Runtime: 0 ms; Memory: 31.9 MB]

--- array/maximize-area-of-square-hole-in-grid.cpp
+++ array/maximize-area-of-square-hole-in-grid.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        hBars.push_back(n+2);
-        hBars.push_back(1);
-        vBars.push_back(m+2);
-        vBars.push_back(1);
-        sort(hBars.begin(),hBars.end());
-         sort(vBars.begin(),vBars.end());
-         long long area=0;
-         long long len=0,bth=0;
-         long long l=1,b=1;
-        for(int i=1;i<hBars.size()-1;i++){
-            if(hBars[i+1]==hBars[i]+1){
-                l++;
-                len=max(len,l);
+    // Helper to find the maximum consecutive sequence length in a sorted vector
+    int getMaxConsecutive(vector<int>& bars) {
+        sort(bars.begin(), bars.end());
+        int max_consecutive = 1;
+        int current_consecutive = 1;
+        
+        for (int i = 1; i < bars.size(); ++i) {
+            if (bars[i] == bars[i - 1] + 1) {
+                current_consecutive++;
+            } else {
+                current_consecutive = 1;
             }
-            else{
-                l=1;
-            }
+            max_consecutive = max(max_consecutive, current_consecutive);
         }
-
-        for(int j=1;j<vBars.size()-1;j++){
-            if(vBars[j+1]==vBars[j]+1){
-                b++;
-                bth=max(bth,b);
-            }
-            else{
-                b=1;
-            }
-        }
-        area=min(len,bth)*min(len,bth);
-        area=max(area,4LL);
-        return area;
+        return max_consecutive;
     }
 
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        // Gap size = number of consecutive bars + 1
+        int maxH = getMaxConsecutive(hBars) + 1;
+        int maxV = getMaxConsecutive(vBars) + 1;
+        
+        int side = min(maxH, maxV);
+        return side * side;
+    }
 };
