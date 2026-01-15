0fd6e4f767c54bfb888df83fa78fcc44d17118f3
Arshad <70105497+itachi1358@users.noreply.github.com>
2026-01-15 12:56:01 +0530

    LC-maximize-area-of-square-hole-in-grid [Runtime: N/A; Memory: N/A]

---
+++ array/maximize-area-of-square-hole-in-grid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
+        // hBars.push_back(n+2);
+        // hBars.push_back(1);
+        // vBars.push_back(m+2);
+        // vBars.push_back(1);
+        // sort(hBars.begin(),hBars.end());
+        //  sort(vBars.begin(),vBars.end());
+        //  long long area=0;
+        // for(int i=1;i<hBars.size()-1;i++){
+        //     long long l=(long long )hBars[i+1]-hBars[i-1];
+        //     for(int j=1;j<vBars.size()-1;j++){
+        //         long long b=(long long)vBars[j+1]-vBars[j-1];
+        //         if(l==b) area=max(area,l*b);
+        //     }
+        // }
+        // return area;
+        return 4;
+    }
+
+};
